88ae2ef1faae4757c3f077c0f64267409c67bc98
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-05-13 00:53:15 +0000

    yuyen

--- src/main/java/Child.java
+++ src/main/java/Child.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
 class Child extends Person {
 
     private Parent parent1;
     private Parent parent2;
-    private ArrayList<Child> siblings; 
+    private Child[] siblings; 
 
     public Child(String name, int age, Parent par1, Parent par2) {
         super(name, age);
         this.parent1 = par1;
         this.parent2 = par2;
-        this.siblings = new ArrayList<>();
+        this.siblings = new Child[0];
     }
     
     public Parent getParent1() {
@@ -20,15 +19,20 @@ class Child extends Person {
         return this.parent2;
     }
     
-    public void setSiblings(ArrayList<Child> siblings) {
+    public void setSiblings(Child[] siblings) {
         this.siblings = siblings;
     }
     
     public Child[] getSiblings() {
-        return this.siblings.toArray(new Child[siblings.size()]);
+        return this.siblings;
     }
 
     public void addSibling(Child child) {
-        this.siblings.add(child);
+        Child[] newSiblings = new Child[this.siblings.length + 1];
+        for (int i = 0; i < this.siblings.length; i++) {
+            newSiblings[i] = this.siblings[i];
+        }
+        newSiblings[this.siblings.length] = child;
+        this.siblings = newSiblings;
     }
 }
